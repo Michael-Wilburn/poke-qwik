a9d2194e85022054d9b285ea10681a238f6e0470
Michael Wilburn <michael-mw@hotmail.com>
2025-06-21 21:08:29 -0300

    SSR - Peticion HTTP

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,9 +1,33 @@
 import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { type DocumentHead, Link, routeLoader$ } from '@builder.io/qwik-city';
+
+export const usePokemonList = routeLoader$(async()=>{
+    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=10`);
+    const data = await resp.json()
+    
+    return data
+})
 
 export default component$(() => {
-  return(
-    <>Hola ssrr</>
+    const pokemonResp = usePokemonList();
+
+    return(
+        <>
+        <div class="flex flex-col">
+            <span class="my-5 text-5xl">Status:</span>
+            <span class="my-5 text-5xl">Page:</span>
+            <span class="my-5 text-5xl">Loading:</span>
+        </div>
+        <div class="mt-10">
+        <Link class="btn btn-primary mr-2">Anteriores</Link>
+        <Link class="btn btn-primary mr-2">Siguientes</Link>
+        </div>
+        <div class="grid grid-cols-6 mt-5">
+            <div>Pokemon</div>
+
+        </div>
+        <div>{JSON.stringify(pokemonResp.value)}</div>
+        </>
   )
 });
 

0389f284c5388fc294569101f2e09996c309c1a2
Michael Wilburn <michael-mw@hotmail.com>
2025-06-21 21:36:02 -0300

    Tipar y mostrar los pokemons

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,15 +1,16 @@
 import { component$ } from '@builder.io/qwik';
 import { type DocumentHead, Link, routeLoader$ } from '@builder.io/qwik-city';
+import { BasicPokemonInfo, PokemonListResponse } from '~/interfaces';
 
-export const usePokemonList = routeLoader$(async()=>{
+export const usePokemonList = routeLoader$<BasicPokemonInfo[]>(async()=>{
     const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=10`);
-    const data = await resp.json()
+    const data = await resp.json() as PokemonListResponse
     
-    return data
+    return data.results
 })
 
 export default component$(() => {
-    const pokemonResp = usePokemonList();
+    const pokemons = usePokemonList();
 
     return(
         <>
@@ -23,10 +24,12 @@ export default component$(() => {
         <Link class="btn btn-primary mr-2">Siguientes</Link>
         </div>
         <div class="grid grid-cols-6 mt-5">
-            <div>Pokemon</div>
-
+            {pokemons.value.map((pokemon)=>(
+            <div key={pokemon.name} class="m-5 flex flex-col justify-center items-center">
+                <span class="capitalize">{pokemon.name}</span>
+            </div>
+            ))}
         </div>
-        <div>{JSON.stringify(pokemonResp.value)}</div>
         </>
   )
 });
